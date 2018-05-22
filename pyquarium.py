b85cb3b970ffaddea31a8c6de4dc8cfebb857b27
yrmus <ed78e1ba5e>
2018-05-22 19:34:07 +0200

    test commit

---
+++ pyquarium.py
@@ -0,0 +1,19 @@
+
+from light import Light
+
+class Pyquarium():
+    def __init__(self):
+        self.light = Light()
+        self.start()
+
+    def start(self):
+        try:
+            while True:
+                self.light.set_color(255, 255, 255)
+        except KeyboardInterrupt:
+            self.light.set_color(255, 255, 255)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    Pyquarium()
