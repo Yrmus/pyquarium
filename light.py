acf068e4c556d5e3cfa56fdc8b9d6a7971bf74df
yrmus <ed78e1ba5e>
2018-05-22 19:35:12 +0200

    Added forgotten import

--- light.py
+++ light.py
@@ -1,4 +1,6 @@
 
+from neopixel import *
+
 LED_COUNT = 150  # Number of LED pixels.
 LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
