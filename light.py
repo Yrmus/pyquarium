7a6faa9c008915589c37fdf5aed1a8a8cb75e32d
yrmus <ed78e1ba5e>
2018-05-27 21:37:54 +0200

    Transfer configuration data to external file

--- light.py
+++ light.py
@@ -1,22 +1,36 @@
-
 from neopixel import *
+import configparser
+#
+# LED_COUNT = 105  # Number of LED pixels.
+# LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+# LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+# LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+# LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+# LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-LED_COUNT = 105  # Number of LED pixels.
-LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 class Light():
     def __init__(self):
-        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS,
-                                       LED_CHANNEL)
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        defaultConfig = config['DEFAULT']
+        self.strip = Adafruit_NeoPixel(defaultConfig['LedCount'],
+                                       defaultConfig['LedPin'],
+                                       defaultConfig['LedFreqHz'],
+                                       defaultConfig['LedDma'],
+                                       defaultConfig['LedInvers'],
+                                       defaultConfig['LedBrightness'],
+                                       defaultConfig['LedChannel'])
         self.strip.begin()
 
     def set_color(self, red, green, blue):
-        """Wipe color across display a pixel at a time."""
         for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, Color(red, green, blue))
         self.strip.show()
+
+    def turn_off(self):
+        pass
+
+
+
